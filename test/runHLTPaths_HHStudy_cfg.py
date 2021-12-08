d5e250a3d54217618b97dc4a8633308c16a3689f
Sebastian Wuchterl <sebastian.wuchterl@cern.ch>
2021-12-08 14:29:09 +0100

    some run config for optimization

--- test/runHLTPaths_HHStudy_cfg.py
+++ test/runHLTPaths_HHStudy_cfg.py
@@ -71,6 +71,12 @@ options.register(
   VarParsing.varType.string,
   'Parameters to load for ROI reco'
 )
+options.register(
+    'histogramJets', False,
+  VarParsing.multiplicity.singleton,
+  VarParsing.varType.bool,
+  'Create histograms of calo and PF jets'
+)
 ## 'maxEvents' is already registered by the Framework, changing default value
 options.setDefault('maxEvents', -1)
 
@@ -174,6 +180,121 @@ elif options.reco == 'HLT_Run3TRK_ROICaloROIPF_Mu':
     process = BTV_roiCalo_roiPF_DeepJet(process)
     update_jmeCalibs = True
 
+elif options.reco == 'HLT_Run3TRK_ROICaloROIPF_Mu_optimized':
+    from HLTrigger.Configuration.customizeHLTforRun3 import *
+    import json
+    process = TRK_newTracking(process)
+    process = MUO_newReco(process)
+    # process = BTV_noCalo_roiPF_DeepCSV(process)
+    process = BTV_roiCalo_roiPF_DeepJet(process)
+    update_jmeCalibs = True
+
+    process.HLT_QuadPFJet98_83_71_15_DoublePFBTagDeepJet_1p3_7p7_VBF1_v8 = cms.Path(
+        process.HLTBeginSequence+
+        process.hltL1sTripleJet927664VBFIorHTTIorDoubleJetCIorSingleJet+
+        process.hltPreQuadPFJet98837115DoublePFBTagDeepCSV1p37p7VBF1+
+        process.HLTAK4CaloJetsSequence+
+        process.hltQuadJet15+
+        process.hltTripleJet50+
+        process.hltDoubleJet65+
+        process.hltSingleJet80+
+        process.hltVBFCaloJetEtaSortedMqq150Deta1p5+
+        # process.HLTFastPrimaryVertexSequenceROIForBTag+
+        # process.HLTBtagDeepCSVSequenceL3ROIForBTag+
+        # process.hltBTagCaloDeepCSV1p56SingleROIForBTag+
+        process.HLTAK4PFJetsSequenceROIForBTag+
+        process.hltPFQuadJetLooseID15ROIForBTag+
+        process.hltPFTripleJetLooseID71ROIForBTag+
+        process.hltPFDoubleJetLooseID83ROIForBTag+
+        process.hltPFSingleJetLooseID98ROIForBTag+
+        process.HLTBtagDeepJetSequencePFROIForBTag+
+        process.hltSelector6PFJetsROIForBTag+
+        process.hltBTagPFDeepJet7p68Double6JetsROIForBTag+
+        process.hltBTagPFDeepJet1p28Single6JetsROIForBTag+
+        process.hltVBFPFJetDeepJetSortedMqq200Detaqq1p5ROIForBTag+
+        process.HLTEndSequence
+    )
+
+    roi_params = {}
+
+    if options.loadROIparamsJson is not None:
+        try:
+            with open(options.loadROIparamsJson, "r") as roi_config_file:
+                roi_params.update( json.load(roi_config_file))
+        except FileNotFoundError as e:
+            print("File {} could not be found. Using default values as fallback".format(roi_config_file))
+            raise ValueError
+
+    roi_defaults = {
+            "beamSpot" : "hltOnlineBeamSpot",
+            "deltaEta" : 0.5,
+            "deltaPhi" : 0.5,
+            "input" : "hltSelectorCentralJets20L1FastJeta",
+            "maxNRegions" : 8,
+            "maxNVertices" : 2,
+            "measurementTrackerName" : "",
+            "mode" : "VerticesFixed",
+            "nSigmaZBeamSpot" : 3.0,
+            "nSigmaZVertex" : 0.0,
+            "originRadius" : 0.3,
+            "precise" : True,
+            "ptMin" : 0.8,
+            "searchOpt" : True,
+            "vertexCollection" : "hltTrimmedPixelVertices",
+            "whereToUseMeasurementTracker" : "Never",
+            "zErrorBeamSpot" : 0.5,
+            "zErrorVetex" : 0.3
+        }
+    process.hltBTaggingRegion = cms.EDProducer("CandidateSeededTrackingRegionsEDProducer",
+        RegionPSet = cms.PSet(
+            beamSpot = cms.InputTag(roi_params.get("beamSpot", roi_defaults["beamSpot"])),
+            deltaEta = cms.double(roi_params.get("deltaEta", roi_defaults["deltaEta"])),
+            deltaPhi = cms.double(roi_params.get("deltaPhi", roi_defaults["deltaPhi"])),
+            input = cms.InputTag(roi_params.get("input", roi_defaults["input"])),
+            maxNRegions = cms.int32(roi_params.get("maxNRegions", roi_defaults["maxNRegions"])),
+            maxNVertices = cms.int32(roi_params.get("maxNVertices", roi_defaults["maxNVertices"])),
+            measurementTrackerName = cms.InputTag(roi_params.get("measurementTrackerName", roi_defaults["measurementTrackerName"])),
+            mode = cms.string(roi_params.get("mode", roi_defaults["mode"])),
+            nSigmaZBeamSpot = cms.double(roi_params.get("nSigmaZBeamSpot", roi_defaults["nSigmaZBeamSpot"])),
+            nSigmaZVertex = cms.double(roi_params.get("nSigmaZVertex", roi_defaults["nSigmaZVertex"])),
+            originRadius = cms.double(roi_params.get("originRadius", roi_defaults["originRadius"])),
+            precise = cms.bool(roi_params.get("precise", roi_defaults["precise"])),
+            ptMin = cms.double(roi_params.get("ptMin", roi_defaults["ptMin"])),
+            searchOpt = cms.bool(roi_params.get("searchOpt", roi_defaults["searchOpt"])),
+            vertexCollection = cms.InputTag(roi_params.get("vertexCollection", roi_defaults["vertexCollection"])),
+            whereToUseMeasurementTracker = cms.string(roi_params.get("whereToUseMeasurementTracker", roi_defaults["whereToUseMeasurementTracker"])),
+            zErrorBeamSpot = cms.double(roi_params.get("zErrorBeamSpot", roi_defaults["zErrorBeamSpot"])),
+            zErrorVetex = cms.double(roi_params.get("zErrorVetex", roi_defaults["zErrorVetex"]))
+            )
+    )
+    process.hltPixelTracksCleanForBTag = cms.EDProducer("TrackWithVertexSelector",
+        copyExtras = cms.untracked.bool(False),
+        copyTrajectories = cms.untracked.bool(False),
+        d0Max = cms.double(999.0),
+        dzMax = cms.double(999.0),
+        etaMax = cms.double(5.0),
+        etaMin = cms.double(0.0),
+        nSigmaDtVertex = cms.double(0.0),
+        nVertices = cms.uint32(2),
+        normalizedChi2 = cms.double(999999.0),
+        numberOfLostHits = cms.uint32(999),
+        numberOfValidHits = cms.uint32(0),
+        numberOfValidPixelHits = cms.uint32(3),
+        ptErrorCut = cms.double(5.0),
+        ptMax = cms.double(9999.0),
+        # ptMin = cms.double(0.8),
+        ptMin =  cms.double(roi_params.get("ptMin", roi_defaults["ptMin"])),
+        quality = cms.string("loose"),
+        rhoVtx = cms.double(0.2),
+        src = cms.InputTag("hltPixelTracks"),
+        timeResosTag = cms.InputTag(""),
+        timesTag = cms.InputTag(""),
+        useVtx = cms.bool(True),
+        vertexTag = cms.InputTag("hltTrimmedPixelVertices"),
+        vtxFallback = cms.bool(True),
+        zetaVtx = cms.double(0.3),
+    )
+
 elif options.reco == 'HLT_Run3TRK_ROICaloROIPF_Mu_oldJECs':
     from HLTrigger.Configuration.customizeHLTforRun3 import *
     process = TRK_newTracking(process)
@@ -189,6 +310,31 @@ elif options.reco == 'HLT_Run3TRK_ROICaloGlobalPF_Mu':
     # process = BTV_noCalo_roiPF_DeepCSV(process)
     process = BTV_roiCalo_globalPF_DeepJet(process)
     update_jmeCalibs = True
+    process.HLT_QuadPFJet98_83_71_15_DoublePFBTagDeepJet_1p3_7p7_VBF1_v8 = cms.Path(
+        process.HLTBeginSequence+
+        process.hltL1sTripleJet927664VBFIorHTTIorDoubleJetCIorSingleJet+
+        process.hltPreQuadPFJet98837115DoublePFBTagDeepCSV1p37p7VBF1+
+        process.HLTAK4CaloJetsSequence+
+        process.hltQuadJet15+
+        process.hltTripleJet50+
+        process.hltDoubleJet65+
+        process.hltSingleJet80+
+        process.hltVBFCaloJetEtaSortedMqq150Deta1p5+
+        # process.HLTFastPrimaryVertexSequenceROIForBTag+
+        # process.HLTBtagDeepCSVSequenceL3ROIForBTag+
+        # process.hltBTagCaloDeepCSV1p56SingleROIForBTag+
+        process.HLTAK4PFJetsSequence+
+        process.hltPFQuadJetLooseID15+
+        process.hltPFTripleJetLooseID71+
+        process.hltPFDoubleJetLooseID83+
+        process.hltPFSingleJetLooseID98+
+        process.HLTBtagDeepJetSequencePF+
+        process.hltSelector6PFJets+
+        process.hltBTagPFDeepJet7p68Double6Jets+
+        process.hltBTagPFDeepJet1p28Single6Jets+
+        process.hltVBFPFJetDeepJetSortedMqq200Detaqq1p5+
+        process.HLTEndSequence
+    )
 
 elif options.reco == 'HLT_Run3TRK_ROICaloGlobalPF_Mu_oldJECs':
     from HLTrigger.Configuration.customizeHLTforRun3 import *
@@ -288,12 +434,13 @@ keepPaths = [
   # 'HLT_*',
   # 'HLT_QuadPFJet*DeepJet*',
 
-  # 'HLT_QuadPFJet98_83_71_15_DoublePFBTagDeepJet_1p3_7p7_VBF1_v8',
+  'HLT_QuadPFJet98_83_71_15_DoublePFBTagDeepJet_1p3_7p7_VBF1_v8',
+  # 'HLT_QuadPFJet70_50_40_30_PFBTagParticleNet_2BTagSum0p65_v1',
 
-  'HLT_*',
-  'Alca_*',
-  'DST_ZeroBias_v2',
-  'DST_Physics_v7',
+  # 'HLT_*',
+  # 'Alca_*',
+  # 'DST_ZeroBias_v2',
+  # 'DST_Physics_v7',
   'Status_OnGPU',
   'Status_OnCPU',
   'HLTriggerFinalPath',
@@ -441,18 +588,18 @@ else:
     # multi-threading settings
     process.options.numberOfThreads = cms.untracked.uint32(options.numThreads if (options.numThreads > 1) else 1)
     process.options.numberOfStreams = cms.untracked.uint32(options.numStreams if (options.numStreams > 1) else 1)
-    process.options.makeTriggerResults = cms.untracked.bool(True)
-    process.hltTrigReport = cms.EDAnalyzer("HLTrigReport",
-        HLTriggerResults = cms.InputTag("TriggerResults","","@currentProcess"),
-        ReferencePath = cms.untracked.string('HLTriggerFinalPath'),
-        ReferenceRate = cms.untracked.double(100.0),
-        reportBy = cms.untracked.string('job'),
-        resetBy = cms.untracked.string('never'),
-        serviceBy = cms.untracked.string('never')
-    )
-    process.HLTAnalyzerEndpath = cms.EndPath(process.hltTrigReport)
-    process.schedule.extend([process.HLTAnalyzerEndpath])
-    process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
+process.options.makeTriggerResults = cms.untracked.bool(True)
+process.hltTrigReport = cms.EDAnalyzer("HLTrigReport",
+    HLTriggerResults = cms.InputTag("TriggerResults","","@currentProcess"),
+    ReferencePath = cms.untracked.string('HLTriggerFinalPath'),
+    ReferenceRate = cms.untracked.double(100.0),
+    reportBy = cms.untracked.string('job'),
+    resetBy = cms.untracked.string('never'),
+    serviceBy = cms.untracked.string('never')
+)
+process.HLTAnalyzerEndpath = cms.EndPath(process.hltTrigReport)
+process.schedule.extend([process.HLTAnalyzerEndpath])
+process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
 
 #Set GT by hand:
 # process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
@@ -523,6 +670,32 @@ if options.runTiming:
     process.FastTimerService.dqmModuleTimeResolution =     1.
     # process.dqmOutput.fileName = cms.untracked.string(options.output)
 
+
+## Output file
+if options.histogramJets:
+    process.TFileService = cms.Service("TFileService",
+       fileName = cms.string("HHStudy.root")
+    )
+    from JMETriggerAnalysis.Common.jetHistogrammer_cfi import jetHistogrammer
+    process.JetHistograms_hltSelectorCentralJets20L1FastJeta = jetHistogrammer.clone(
+        srcCaloJets = 'hltSelectorCentralJets20L1FastJeta',
+        srcPFJets   = 'hltAK4PFJetsROIForBTag',
+        srcGenJets   = 'DUMMY',
+        JetType      = 'CaloJet',
+    )
+    process.JetHistograms_hltAK4PFJetsROIForBTag = process.JetHistograms_hltSelectorCentralJets20L1FastJeta.clone(
+        JetType = "PFJet"
+    )
+    # process.VertexHistograms_hltTrimmedPixelVertices = JetHistogrammer.clone(src = 'hltTrimmedPixelVertices')
+    # process.VertexHistograms_hltVerticesPF = JetHistogrammer.clone(src = 'hltVerticesPF')
+    # process.VertexHistograms_hltVerticesPFFilter = JetHistogrammer.clone(src = 'hltVerticesPFFilter')
+    #
+    process.jetMonitoringSeq = cms.Sequence(
+       process.JetHistograms_hltSelectorCentralJets20L1FastJeta
+       *process.JetHistograms_hltAK4PFJetsROIForBTag
+    )
+    process.jetMonitoringEndPath = cms.EndPath(process.jetMonitoringSeq)
+    process.schedule.extend([process.jetMonitoringEndPath])
 
 # dump content of cms.Process to python file
 if options.dumpPython is not None:
